8ee883091abf7d58b1dd68fe9cf63c24bf94a6bb
Zihao Wang <zihaowangzihao@gmail.com>
2023-01-19 20:16:37 -0500

    Create Page

--- client/src/components/pages/CreatePage.js
+++ client/src/components/pages/CreatePage.js
@@ -5,28 +5,9 @@ import jwt_decode from "jwt-decode";
 const CreatePage = (props) => {
   return (
     <>
-      <div class="u-flexColumn">
-        <div class="u-flex">
-          <div>
-            <a href="http://localhost:5050/Profile">
-              <img src={profile} class="profile" />
-            </a>
-          </div>
-          <div>
-            <a href="http://localhost:5050/Home">
-              <img src={home} class="home" />
-            </a>
-          </div>
-          <div>
-            <a href="http://localhost:5050/CreatePage">
-              <img src={create} class="create" />
-            </a>
-          </div>
-          <div>
-            <a href="http://localhost:5050/Friends">
-              <img src={friends} class="friends" />
-            </a>
-          </div>
+      <div>
+        <div>
+          <h1>Create New Message</h1>
         </div>
       </div>
     </>
