ffd5153a9387f269fe3f4ab4b35fe096356bbb67
martaylo10 <119651661+martaylo10@users.noreply.github.com>
2023-01-20 19:27:14 -0500

    CreatePage edits

--- client/src/components/pages/CreatePage.js
+++ client/src/components/pages/CreatePage.js
@@ -1,17 +1,73 @@
 import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import jwt_decode from "jwt-decode";
+import profile from "../../public/profile.png";
+ 
+let text = ""
+let image = ""
+
+const EmojiMenu = (props) => {
+    return (
+    <>
+         <div>
+            <h1>Emoji menu</h1>
+        </div>
+    </>
+    );
+};
+
+const InputText = (props) => {
+    const handleChange = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+    <>
+        <div>
+            <h1>User Text</h1>
+            <form>
+                <label>
+                    message:
+                    <input type="text" />
+                </label>
+            </form>
+        </div>
+    </>
+    );
+};
+const NewMessage = (props) => {
+    return (
+        <>
+            <div class="container">
+                <div class="center">
+                <img src={profile} alt="user image"/>
+                </div>
+            </div>
+        </>
+    );
+};
 
 const CreatePage = (props) => {
-  return (
+    return (
     <>
-      <div>
-        <div>
-          <h1>Create a new message</h1>
+        <div class="u-flexColumn">
+            <div class="heading">
+                <h1>Create New Message</h1>
+            </div>
+            <div class="u-flex">
+                <div>
+                    <EmojiMenu/>
+                    <InputText/>
+                </div>
+                <div class="u-flex-justifyCenter">
+                    <NewMessage
+                        uText = {text}
+                        uImage = {image} />
+                </div>
+            </div>
         </div>
-      </div>
     </>
-  );
+    );
 };
 
 export default CreatePage;
